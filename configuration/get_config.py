143aaa3b046171db61f19e5ec541c1d83361d7f3
SEO\VRudenko <viktoriia.rudenko@onseo.biz>
2024-12-28 16:27:56 +0200

    update project

--- configuration/get_config.py
+++ configuration/get_config.py
@@ -4,7 +4,7 @@ from configuration.env import get_environment
 env = get_environment()
 
 def get_config(env=env):
-    with open('C:/Users/1111vrudenko/PycharmProjects/PythonProjectAPI1/configuration/config.yaml', 'r') as f:
+    with open('/PycharmProjects/PythonProjectAPI1/configuration/config.yaml', 'r') as f:
         config_data = yaml.load(f, Loader=yaml.FullLoader)
         config = config_data[env]
         return config
